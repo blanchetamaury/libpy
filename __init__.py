ee6a4f55c37f1847af8f67f7ddd63380d788077d
momori85 <177555279+momori85@users.noreply.github.com>
2025-07-06 22:06:57 +0200

    feat: new fonction: stddev, factoriel_rec and factoriel_rec + (factoriel_loop:private)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 """API publique de mon_package."""
 
-from .core import clamp, import_csv, export_csv, lerp, mean, sort_insertion, median
+from .core import clamp, import_csv, export_csv, lerp, mean, sort_insertion, median, stddev, factoriel_rec, factoriel_iter
 
-__all__ = ["clamp", "import_csv", "export_csv", "lerp", "mean", "sort_insertion", "median"]
+__all__ = ["clamp", "import_csv", "export_csv", "lerp", "mean", "sort_insertion", "median", "stddev", "factoriel_rec", "factoriel_iter"]
--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 import csv as csv
+import math as mt
 
 def clamp(value, min, max):
     """Borne value dans l'intervalle min et max.
@@ -86,6 +87,14 @@ def mean(iterable):
     return count
 
 def sort_insertion(iterable):
+    """trie la liste par insertion.
+
+    Args:
+        iterable: La liste a trier.
+    
+    Returns:
+        la liste trier.
+    """
     for i in range(1, len(iterable)):
         key = iterable[i]
         j = i - 1
@@ -96,6 +105,18 @@ def sort_insertion(iterable):
     return iterable
 
 def median(iterable):
+    """ trouve la median des list pair et impair.
+
+    Args:
+        iterable: la liste ou il faut trouver la medianne.
+    
+    Returns:
+        renvoie la medianne, si la liste est pair c'est un nombre entre les deux nombre au milieu de la liste et en float 
+        et si elle est inpair c'est le nombre au milieur.
+    
+    Raises:
+        valueError: median() d'un iterable vide.
+    """
     value = sort_insertion(list(iterable))
     size = len(value)
     if size == 0:
@@ -106,3 +127,61 @@ def median(iterable):
     else:
         i = size // 2
         return float(value[i])
+
+def stddev(iterable, sample=False):
+    """Fait un ecart type sur un échantillon ou une population.
+
+    Args:
+        iterable: la liste qui est un echantillon ou une population.
+        sample: si il est sur False c'est une population et si il est sur True c'est un echantillon.
+    
+    Returns:
+        renvoie l'ecart type de la liste.
+    Raises:
+        ValueError: sttdev() necessite au moins 1 (ou 2) valeur.
+    """
+    nums = [float(x) for x in iterable]
+    j = 0
+    result = 0
+    m = mean(nums)
+    size = len(nums)
+    if (size == 0 or (sample and size == 1)):
+        raise ValueError("stddev() nécessite au moins 1 (ou 2) valeur")
+    while j < size:
+        nums[j] = nums[j] - m
+        result += nums[j] * nums[j]
+        j += 1
+    divisor = size - 1 if sample else size
+    result = result / divisor
+    return mt.sqrt(result)
+
+def factoriel_iter(n):
+    """Factoriel de n (≥ 0) via iteration.
+    """
+    if not isinstance(n, int):
+        raise TypeError("factorial_iter() attend un entier")
+    if n < 0:
+        raise ValueError("factorial_iter() n'est pas défini pour n < 0")
+    i = 1
+    value = 1
+    while i <= n:
+        value *= i
+        i += 1
+    return value
+
+def factoriel_loop(n, i, value):
+    if i <= n:
+        value = factoriel_loop(n, i + 1, value * i)
+    return value
+
+def factoriel_rec(n):
+    """Factoriel de n (≥ 0) via récursion terminale.
+
+    Attention : CPython ne fait pas d’optimisation TCO,
+    donc RecursionError pour de très grands n.
+    """
+    if not isinstance(n, int):
+        raise TypeError("factorial_iter() attend un entier")
+    if n < 0:
+        raise ValueError("factorial_iter() n'est pas défini pour n < 0")
+    return factoriel_loop(n, 1, 1)
